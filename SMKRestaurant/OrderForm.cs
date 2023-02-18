76e38c1415b13e23bd6b4564b90b7532d424b949
Hafiz Caniago <hafizcode02@gmail.com>
2023-02-18 19:18:05 +0700

    add list order to datagridview

--- SMKRestaurant/OrderForm.cs
+++ SMKRestaurant/OrderForm.cs
@@ -12,6 +12,7 @@ namespace SMKRestaurant
         DataTable dt2 = new DataTable(); // DG Order List
         DataTable dt3 = new DataTable(); // Temporary DT
         int MenuID = 0;
+        bool equalStatus = false;
         public OrderForm(String employeeID)
         {
             InitializeComponent();
@@ -47,23 +48,56 @@ namespace SMKRestaurant
             {
                 if (MenuID != 0)
                 {
-                    dt3.Clear();
-                    koneksi.select("SELECT * FROM MsMenu WHERE Id='" + MenuID + "'");
+                    dt3.Clear(); // Clear Datatable
+                    koneksi.select("SELECT * FROM MsMenu WHERE Id='" + MenuID + "'"); // Get Data
                     koneksi.adp.Fill(dt3);
 
-                    if (dt3.Rows.Count != 0)
+                    if (dt3.Rows.Count != 0) // Checking Not 0
                     {
-                        DataRow dtr = dt3.Rows[0];
-                        int price = int.Parse(dtr[2].ToString()) * int.Parse(txtQty.Text);
-                        dgOrderList.Rows.Add(
-                            dtr[0].ToString(),
-                            dtr[1].ToString(),
-                            txtQty.Text,
-                            dtr[4].ToString(),
-                            dtr[5].ToString(),
-                            dtr[2].ToString(),
-                            price
-                        );
+                        DataRow dtr = dt3.Rows[0]; // Get Data in Row
+                        int price = int.Parse(dtr[2].ToString()) * int.Parse(txtQty.Text); // Calculate Price
+
+                        int maxCount = dgOrderList.RowCount; // Max Iteration in DataGridView
+                        int count = 0;
+
+                        foreach (DataGridViewRow row in dgOrderList.Rows)
+                        {
+                            count += 1;
+                            if (count == maxCount) // This Line is Important, because Datagridview also read a last row, which is empty row
+                            {
+                                break;
+                            }
+
+                            // Check Equals Data For Update
+                            if (row.Cells["Column1"].Value.ToString() == dtr[0].ToString())
+                            {
+                                equalStatus = true;
+                                row.Cells[2].Value = txtQty.Text;
+                                row.Cells[6].Value = price;
+
+                                break;
+                            } // Give status = false
+                            else
+                            {
+                                equalStatus = false;
+                            }
+
+                        }
+                        count = 0; // reset counter
+
+                        if (equalStatus == false) // add new row, if not find equal data on order list
+                        {
+                            dgOrderList.Rows.Add(
+                                dtr[0].ToString(),
+                                dtr[1].ToString(),
+                                txtQty.Text,
+                                dtr[4].ToString(),
+                                dtr[5].ToString(),
+                                dtr[2].ToString(),
+                                price
+                            );
+                        }
+
                     }
                 }
             }
